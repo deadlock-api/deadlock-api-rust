5d39dd9e1f338031d2f1d115a299aea99b7db525
Manuel Raimann <raimannma@outlook.de>
2025-02-25 01:47:29 +0100

    Update API documentation with game description and Discord contact info

--- src/api_doc.rs
+++ src/api_doc.rs
@@ -6,6 +6,12 @@ use utoipa::{Modify, OpenApi};
     info(
         title = "Deadlock API",
         version = "0.1.0",
+        description = r#"
+API for the Game [Deadlock](https://store.steampowered.com/app/1422450)
+
+deadlock-api.com is not endorsed by Valve and does not reflect the views or opinions of Valve or anyone officially involved in producing or managing Valve properties. Valve and all associated properties are trademarks or registered trademarks of Valve Corporation
+        "#,
+        contact(name = "Deadlock API  - Discord", url = "https://discord.gg/XMF9Xrgfqu"),
     ),
     modifiers(&SecurityAddon),
 )]
