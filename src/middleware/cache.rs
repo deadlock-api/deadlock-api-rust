e36aa35bfa78cdfabf8b17c0ce3d23d6daa70695
Manuel Raimann <raimannma@outlook.de>
2025-02-27 10:57:02 +0100

    Remove `must-revalidate` directive from CacheControlMiddleware

--- src/middleware/cache.rs
+++ src/middleware/cache.rs
@@ -11,7 +11,6 @@ use tower_service::Service;
 #[derive(Debug, Clone)]
 pub struct CacheControlMiddleware {
     max_age: Duration,
-    must_revalidate: bool,
     private: bool,
 }
 
@@ -20,15 +19,8 @@ impl CacheControlMiddleware {
     pub fn new(max_age: Duration) -> Self {
         Self {
             max_age,
-            must_revalidate: false,
             private: false,
         }
-    }
-
-    /// Sets the `must-revalidate` directive.
-    pub fn must_revalidate(mut self) -> Self {
-        self.must_revalidate = true;
-        self
     }
 
     /// Sets the `private` directive.
@@ -40,9 +32,6 @@ impl CacheControlMiddleware {
     fn header_value(&self) -> HeaderValue {
         let mut header_value = String::new();
         write!(&mut header_value, "max-age={}", self.max_age.as_secs()).ok();
-        if self.must_revalidate {
-            write!(&mut header_value, ", must-revalidate").ok();
-        }
         if self.private {
             write!(&mut header_value, ", private").ok();
         } else {
@@ -135,25 +124,6 @@ mod tests {
     }
 
     #[tokio::test]
-    async fn test_must_revalidate() {
-        let layer = CacheControlMiddleware::new(Duration::from_secs(60)).must_revalidate();
-        let app = Router::new().route("/", get(test_handler)).layer(layer);
-
-        let response = app
-            .oneshot(Request::builder().uri("/").body(Body::empty()).unwrap())
-            .await
-            .unwrap();
-
-        assert_eq!(response.status(), StatusCode::OK);
-        assert_eq!(
-            response
-                .headers()
-                .get(axum::http::header::CACHE_CONTROL)
-                .unwrap(),
-            "max-age=60, must-revalidate, public"
-        );
-    }
-    #[tokio::test]
     async fn test_private() {
         let layer = CacheControlMiddleware::new(Duration::from_secs(60)).private();
         let app = Router::new().route("/", get(test_handler)).layer(layer);
@@ -174,9 +144,7 @@ mod tests {
 
     #[tokio::test]
     async fn test_all_options() {
-        let layer = CacheControlMiddleware::new(Duration::from_secs(60))
-            .must_revalidate()
-            .private();
+        let layer = CacheControlMiddleware::new(Duration::from_secs(60)).private();
         let app = Router::new().route("/", get(test_handler)).layer(layer);
 
         let response = app
@@ -190,7 +158,7 @@ mod tests {
                 .headers()
                 .get(axum::http::header::CACHE_CONTROL)
                 .unwrap(),
-            "max-age=60, must-revalidate, private"
+            "max-age=60, private"
         );
     }
 }
