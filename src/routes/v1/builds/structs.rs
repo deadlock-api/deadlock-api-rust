211c57e957808fb285db9fc02f280b1c57e99cf7
Manuel Raimann <raimannma@outlook.de>
2025-03-20 17:05:50 +0100

    Fix

--- src/routes/v1/builds/structs.rs
+++ src/routes/v1/builds/structs.rs
@@ -59,13 +59,9 @@ pub struct BuildHero {
 #[serde(rename_all = "snake_case")]
 pub struct Build {
     pub hero_build: BuildHero,
-    #[serde(default)]
-    pub num_favorites: u32,
-    #[serde(default)]
-    pub num_ignores: u32,
-    #[serde(default)]
-    pub num_reports: u32,
-    #[serde(default)]
-    pub num_weekly_favorites: u32,
+    pub num_favorites: Option<u32>,
+    pub num_ignores: Option<u32>,
+    pub num_reports: Option<u32>,
+    pub num_weekly_favorites: Option<u32>,
     pub rollup_category: Option<u32>,
 }
