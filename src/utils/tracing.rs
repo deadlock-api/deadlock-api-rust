bb6dd3a41de3a5e8568647f3f5d221fe221b3e07
Manuel Raimann <raimannma@outlook.de>
2025-03-27 13:03:10 +0100

    Update tracing filter to include reqwest at warn level

--- src/utils/tracing.rs
+++ src/utils/tracing.rs
@@ -4,7 +4,7 @@ use tracing_subscriber::util::SubscriberInitExt;
 
 pub fn init_tracing() {
     let env_filter = EnvFilter::try_from_default_env().unwrap_or(EnvFilter::new(
-        "debug,hyper_util=warn,tower_http=info,rustls=warn,sqlx=warn",
+        "debug,hyper_util=warn,tower_http=info,reqwest=warn,rustls=warn,sqlx=warn",
     ));
     let fmt_layer = tracing_subscriber::fmt::layer();
 
