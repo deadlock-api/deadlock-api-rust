5ed438ef0d2a6dc9c549f7d576d7186171bf3bc4
Manuel Raimann <raimannma@outlook.de>
2025-04-12 01:18:17 +0200

    Implement Integration Tests

---
+++ tests/health.rs
@@ -0,0 +1,23 @@
+use deadlock_api_rust::run_api;
+use reqwest::StatusCode;
+
+#[tokio::test]
+async fn test_load_app_state() {
+    // Start app
+    let handle = tokio::spawn(run_api());
+
+    // Check health
+    let health = tryhard::retry_fn(|| reqwest::get("http://localhost:3000/v1/info/health"))
+        .retries(30)
+        .fixed_backoff(tokio::time::Duration::from_secs(1))
+        .await
+        .expect("Failed to get health");
+
+    let status = health.status();
+    handle.abort_handle().abort();
+
+    match status {
+        StatusCode::OK => println!("{:#?}", health.text().await),
+        status => panic!("App is not healthy: {status}"),
+    }
+}
