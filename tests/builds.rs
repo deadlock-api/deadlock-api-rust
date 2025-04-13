a52943a41b28d1b32a8e306ee3fb8fa6cd302e9b
Manuel Raimann <raimannma@outlook.de>
2025-04-13 19:03:15 +0200

    Add test for builds endpoint and Fix build sorting in edge cases

---
+++ tests/builds.rs
@@ -0,0 +1,142 @@
+#![allow(clippy::too_many_arguments)]
+
+mod utils;
+use deadlock_api_rust::routes::v1::builds::query::{
+    BuildsSearchQuerySortBy, BuildsSearchQuerySortDirection,
+};
+use deadlock_api_rust::routes::v1::builds::structs::Build;
+use itertools::Itertools;
+use rstest::rstest;
+
+#[rstest]
+#[tokio::test]
+async fn test_builds(
+    #[values(None, Some(10))] limit: Option<usize>,
+    #[values(None, Some(15))] hero_id: Option<u32>,
+    #[values(None, Some(74963216), Some(123031))] author_id: Option<u32>,
+    #[values(
+        None,
+        Some(BuildsSearchQuerySortBy::UpdatedAt),
+        Some(BuildsSearchQuerySortBy::Version)
+    )]
+    sort_by: Option<BuildsSearchQuerySortBy>,
+    #[values(None, Some(BuildsSearchQuerySortDirection::Asc))] sort_direction: Option<
+        BuildsSearchQuerySortDirection,
+    >,
+    #[values(None, Some("Lash"))] search_name: Option<&str>,
+    #[values(None, Some("AverageJonas"))] search_description: Option<&str>,
+    #[values(None, Some(true), Some(false))] only_latest: Option<bool>,
+    #[values(None, Some(6))] language: Option<u32>,
+    #[values(None, Some(132494))] build_id: Option<u32>,
+) {
+    let mut queries = vec![];
+    if let Some(limit) = limit {
+        queries.push(("limit", limit.to_string()));
+    }
+    if let Some(hero_id) = hero_id {
+        queries.push(("hero_id", hero_id.to_string()));
+    }
+    if let Some(account_id) = author_id {
+        queries.push(("author_id", account_id.to_string()));
+    }
+    if let Some(sort_by) = sort_by {
+        queries.push(("sort_by", sort_by.to_string()));
+    }
+    if let Some(sort_direction) = sort_direction {
+        queries.push(("sort_direction", sort_direction.to_string()));
+    }
+    if let Some(search_name) = search_name {
+        queries.push(("search_name", search_name.to_string()));
+    }
+    if let Some(search_description) = search_description {
+        queries.push(("search_description", search_description.to_string()));
+    }
+    if let Some(only_latest) = only_latest {
+        queries.push(("only_latest", only_latest.to_string()));
+    }
+    if let Some(language) = language {
+        queries.push(("language", language.to_string()));
+    }
+    if let Some(build_id) = build_id {
+        queries.push(("build_id", build_id.to_string()));
+    }
+    let queries = queries
+        .iter()
+        .map(|(k, v)| (*k, v.as_str()))
+        .collect::<Vec<_>>();
+    let response = utils::request_endpoint("/v1/builds", queries).await;
+    let builds: Vec<Build> = response.json().await.expect("Failed to parse response");
+
+    let sort_by = sort_by.unwrap_or(BuildsSearchQuerySortBy::WeeklyFavorites);
+    let sort_direction = sort_direction.unwrap_or(BuildsSearchQuerySortDirection::Desc);
+    let builds_sorted = if sort_direction == BuildsSearchQuerySortDirection::Desc {
+        builds.iter().rev().collect::<Vec<_>>()
+    } else {
+        builds.iter().collect::<Vec<_>>()
+    };
+
+    fn get_sort_key(sort_by: BuildsSearchQuerySortBy, build: &Build) -> u32 {
+        match sort_by {
+            BuildsSearchQuerySortBy::WeeklyFavorites => build.num_weekly_favorites,
+            BuildsSearchQuerySortBy::Favorites => build.num_favorites,
+            BuildsSearchQuerySortBy::Ignores => build.num_ignores,
+            BuildsSearchQuerySortBy::Reports => build.num_reports,
+            BuildsSearchQuerySortBy::Version => build.hero_build.version.into(),
+            BuildsSearchQuerySortBy::UpdatedAt => {
+                (build.hero_build.last_updated_timestamp as u32).into()
+            }
+        }
+        .unwrap_or_default()
+    }
+
+    let is_sorted = builds_sorted.is_sorted_by_key(|k| get_sort_key(sort_by, k));
+    assert!(
+        is_sorted,
+        "Builds are not sorted by {} in {} order",
+        sort_by, sort_direction
+    );
+
+    if let Some(limit) = limit {
+        assert!(builds.len() <= limit);
+    }
+    if only_latest.unwrap_or_default() {
+        assert_eq!(
+            builds
+                .iter()
+                .unique_by(|a| a.hero_build.hero_build_id)
+                .count(),
+            builds.len()
+        );
+    }
+    for build in &builds {
+        let hero_build = &build.hero_build;
+        if let Some(hero_id) = hero_id {
+            assert_eq!(hero_build.hero_id, hero_id);
+        }
+        if let Some(build_id) = build_id {
+            assert_eq!(hero_build.hero_build_id, build_id);
+        }
+        if let Some(account_id) = author_id {
+            assert_eq!(hero_build.author_account_id, account_id);
+        }
+        if let Some(language) = language {
+            assert_eq!(build.hero_build.language, language);
+        }
+        if let Some(search_name) = search_name {
+            assert!(
+                hero_build
+                    .name
+                    .to_lowercase()
+                    .contains(&search_name.to_lowercase())
+            );
+        }
+        if let Some(search_description) = search_description {
+            assert!(
+                hero_build
+                    .name
+                    .to_lowercase()
+                    .contains(&search_description.to_lowercase())
+            );
+        }
+    }
+}
