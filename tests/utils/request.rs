d06615842403503964675d61d430dec6bc08ab4b
Manuel Raimann <raimannma@outlook.de>
2025-05-02 21:17:57 +0200

    Refactor endpoint URL construction to simplify query string handling using `querystring`.

--- tests/utils/request.rs
+++ tests/utils/request.rs
@@ -1,15 +1,14 @@
 use crate::utils;
 use reqwest::Response;
-use std::str::FromStr;
-use url::Url;
 
 pub async fn request_endpoint(
     endpoint: &str,
     query_args: impl IntoIterator<Item = (&str, &str)>,
 ) -> Response {
-    let mut url: Url = Url::from_str(&format!("http://api:3000{endpoint}")).unwrap();
-    for (name, val) in query_args {
-        url.query_pairs_mut().append_pair(name, val);
+    let mut url = format!("http://api:3000{endpoint}");
+    let query = querystring::stringify(query_args.into_iter().collect());
+    if !query.is_empty() {
+        url = format!("{url}?{query}");
     }
     let response = reqwest::get(url).await.expect("Failed to get response");
     utils::check_response(&response);
