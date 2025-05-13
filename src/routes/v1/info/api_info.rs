a109c1d4709365291302db3e908043e111c548a4
Manuel Raimann <raimannma@outlook.de>
2025-05-13 23:25:53 +0200

    Remove `missed_matches` field and associated query logic
    Deleted the `missed_matches` field from the `APIInfo` struct and removed its corresponding ClickHouse query and usage.

--- src/routes/v1/info/api_info.rs
+++ src/routes/v1/info/api_info.rs
@@ -5,7 +5,7 @@ use axum::response::IntoResponse;
 use cached::TimedCache;
 use cached::proc_macro::cached;
 use clickhouse::Row;
-use futures::future::join3;
+use futures::future::join;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use utoipa::ToSchema;
@@ -40,16 +40,6 @@ WITH fetched_matches AS (
 )
 SELECT COUNT() as fetched_matches_per_day
 FROM fetched_matches
-"#;
-
-const MISSED_MATCHES_QUERY: &str = r#"
-SELECT COUNT(DISTINCT match_id)
-FROM player_match_history
-WHERE toDateTime(start_time) BETWEEN '2025-05-01' AND now() - INTERVAL '2 hours'
-    AND match_mode IN ('Ranked', 'Unranked')
-    AND game_mode = 'Normal'
-    AND match_id NOT IN (SELECT match_id FROM match_salts)
-    AND match_id NOT IN (SELECT match_id FROM match_info)
 "#;
 
 #[derive(Deserialize, Row)]
@@ -93,8 +83,6 @@ impl From<TableSizeRow> for TableSize {
 pub struct APIInfo {
     /// The number of matches fetched in the last 24 hours.
     pub fetched_matches_per_day: Option<u64>,
-    /// The number of matches that have not been fetched.
-    pub missed_matches: Option<u64>,
     /// The sizes of all tables in the database.
     pub table_sizes: Option<HashMap<String, TableSize>>,
 }
@@ -106,19 +94,17 @@ pub struct APIInfo {
     sync_writes = "default"
 )]
 pub async fn fetch_ch_info(ch_client: &clickhouse::Client) -> APIInfo {
-    let (table_sizes, fetched_matches_per_day, missed_matches) = join3(
+    let (table_sizes, fetched_matches_per_day) = join(
         ch_client
             .query(TABLE_SIZES_QUERY)
             .fetch_all::<TableSizeRow>(),
         ch_client
             .query(FETCHED_MATCHES_LAST_24H_QUERY)
             .fetch_one::<u64>(),
-        ch_client.query(MISSED_MATCHES_QUERY).fetch_one::<u64>(),
     )
     .await;
     APIInfo {
         fetched_matches_per_day: fetched_matches_per_day.ok(),
-        missed_matches: missed_matches.ok(),
         table_sizes: table_sizes.ok().map(|v| {
             v.into_iter()
                 .map(|row| (row.table.clone(), row.into()))
