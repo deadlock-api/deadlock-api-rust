fc0482558ea94dd24a65b16b5644dc63ed045fc3
Manuel Raimann <raimannma@outlook.de>
2025-05-18 15:11:34 +0200

    Adjust rate limit thresholds for live_url endpoint
    Increased `key_limit` and `global_limit` thresholds to better accommodate higher traffic demands. This change aims to reduce false positives on rate limiting while maintaining system stability and preventing abuse.

--- src/routes/v1/matches/live_url.rs
+++ src/routes/v1/matches/live_url.rs
@@ -96,8 +96,8 @@ pub async fn live_url(
             "spectate",
             &[
                 RateLimitQuota::ip_limit(10, Duration::from_secs(30 * 60)),
-                RateLimitQuota::key_limit(10, Duration::from_secs(60)),
-                RateLimitQuota::global_limit(10, Duration::from_secs(10)),
+                RateLimitQuota::key_limit(60, Duration::from_secs(60)),
+                RateLimitQuota::global_limit(100, Duration::from_secs(10)),
             ],
         )
         .await?;
