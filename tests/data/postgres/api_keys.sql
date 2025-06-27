eb7d0d2ef4a40592a1e70847f2c17e931466a3d0
Manuel Raimann <raimannma@outlook.de>
2025-06-27 22:16:04 +0200

    Revert "fix: Update SQL scripts to remove public schema references and adjust test API key"
    This reverts commit fd5e5f7a77336c311a7aeecf1f1a366c0b39a455.

--- tests/data/postgres/api_keys.sql
+++ tests/data/postgres/api_keys.sql
@@ -1,26 +1,28 @@
-create table api_keys
+create table public.api_keys
 (
-    key            uuid      default gen_random_uuid() not null primary key,
-    created_at     timestamp default current_timestamp,
-    comment        text
-        constraint api_keys_pk unique,
-    data_access    boolean   default false             not null,
-    disabled       boolean   default false             not null,
-    esports_ingest boolean   default false             not null
+    key uuid default gen_random_uuid() not null primary key,
+    created_at timestamp default current_timestamp,
+    comment text constraint api_keys_pk unique,
+    data_access boolean default false not null,
+    disabled boolean default false not null,
+    esports_ingest boolean default false not null
 );
 
-insert into api_keys (key, comment, data_access, disabled, esports_ingest)
+insert into public.api_keys (
+    key, comment, data_access, disabled, esports_ingest
+)
 values ('fffd6bfd-2be9-4b7e-ab76-a9d1dca19b64', 'Test Key', true, false, false);
 
-create table api_key_limits
+create table public.api_key_limits
 (
-    key         uuid                                  not null
-        constraint limits_key_fkey references public.api_keys,
+    key         uuid                                  not null constraint limits_key_fkey references public.api_keys,
     path        text                                  not null,
     rate_limit  integer  default 10                   not null,
     rate_period interval default '00:00:01'::interval not null,
     constraint limits_pkey primary key (key, path)
 );
 
-insert into api_key_limits (key, path, rate_limit, rate_period)
+insert into public.api_key_limits (
+    key, path, rate_limit, rate_period
+)
 values ('fffd6bfd-2be9-4b7e-ab76-a9d1dca19b64', 'sql', 100, '00:00:01'::interval);
