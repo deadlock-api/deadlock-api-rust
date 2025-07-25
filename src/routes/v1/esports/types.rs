caed7d5de90f3661a9c2d8df8ae9e4051f331604
Manuel Raimann <raimannma@outlook.de>
2025-07-25 10:33:44 +0200

    fix: Add documentation for ESportsMatch struct fields

--- src/routes/v1/esports/types.rs
+++ src/routes/v1/esports/types.rs
@@ -16,13 +16,22 @@ pub(super) enum ESportsMatchStatus {
 
 #[derive(Debug, Clone, Serialize, Deserialize, ToSchema, sqlx::FromRow)]
 pub(super) struct ESportsMatch {
+    /// The provider of the match data. Some string that identifies the source of the data.
     pub(super) provider: String,
+    /// If you want to update an existing match, you can provide an update id.
     pub(super) update_id: Option<Uuid>,
+    /// Valve's match id of the match.
     pub(super) match_id: Option<i64>,
+    /// The name of the first team.
     pub(super) team0_name: Option<String>,
+    /// The name of the second team.
     pub(super) team1_name: Option<String>,
+    /// The name of the tournament.
     pub(super) tournament_name: Option<String>,
+    /// The stage of the tournament.
     pub(super) tournament_stage: Option<String>,
+    /// The scheduled date of the match.
     pub(super) scheduled_date: Option<DateTime<Utc>>,
+    /// The status of the match, e.g. live, completed, scheduled, cancelled.
     pub(super) status: Option<ESportsMatchStatus>,
 }
