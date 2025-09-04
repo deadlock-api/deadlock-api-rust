1bf405c196bc67124dd49845097777b48eccec84
Manuel Raimann <raimannma@outlook.de>
2025-09-04 14:27:37 +0200

    fix: Update MMR calculation to use exponential moving average for improved accuracy

--- src/routes/v1/players/mmr/mod.rs
+++ src/routes/v1/players/mmr/mod.rs
@@ -1,11 +1,13 @@
 mod batch;
 pub mod mmr_history;
 
+use std::time::Duration;
 use utoipa::OpenApi;
 use utoipa_axum::router::OpenApiRouter;
 use utoipa_axum::routes;
 
 use crate::context::AppState;
+use crate::middleware::cache::CacheControlMiddleware;
 
 #[derive(OpenApi)]
 #[openapi(tags((name = "MMR", description = "
@@ -17,9 +19,8 @@ This is how we calculate a player MMR.
 
 1. We take the average badge of the team the player was on in a match.
 2. We convert the badge to a MMR score using the formula: `(intDiv(badge, 10) - 1) * 6 + (badge % 10)`
-3. We calculate a weight for the match using the formula: `if(party = 0, 4.0, 1.0) / pow(row_number() OVER (ORDER BY match_id DESC), 0.4)`
-4. We calculate a weighted average of the last 20 matches using the formula: `sum(mmr_score * weight) / sum(weight)`
-5. We convert the MMR score back to a badge using the formula: `10 * intDiv(mmr_score, 6) + 11 + modulo(mmr_score, 6)`
+3. We do a exponential moving average (EMA) of the last 50 matches to get the player's MMR score.
+4. We convert the MMR score back to a badge using the formula: `10 * intDiv(mmr_score, 6) + 1 + mmr_score % 6`
 
 ### Rate Limits:
 | Type | Limit |
