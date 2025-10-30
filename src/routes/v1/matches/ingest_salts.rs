1867c349c946812e45ee2d01af6a849278c32a9f
Manuel Raimann <raimannma@outlook.de>
2025-10-30 19:13:25 +0100

    feat: Enhance salt validation by integrating ClickHouse checks and caching results

--- src/routes/v1/matches/ingest_salts.rs
+++ src/routes/v1/matches/ingest_salts.rs
@@ -4,7 +4,11 @@ use axum::Json;
 use axum::extract::State;
 use axum::http::HeaderMap;
 use axum::response::IntoResponse;
+use cached::TimedCache;
+use cached::proc_macro::cached;
+use clickhouse::Row;
 use futures::StreamExt;
+use serde::Deserialize;
 use serde_json::json;
 use tracing::{debug, warn};
 
@@ -73,7 +77,8 @@ pub(super) async fn ingest_salts(
         futures::stream::iter(match_salts)
             .map(|salt| {
                 let steam_client = state.steam_client.clone();
-                async move { validate_salt(&steam_client, &salt).await }
+                let ch_client = state.ch_client.clone();
+                async move { validate_salt(&steam_client, &ch_client, &salt).await }
             })
             .buffer_unordered(10)
             .filter_map(core::future::ready)
@@ -93,14 +98,57 @@ pub(super) async fn ingest_salts(
     Ok(Json(json!({ "status": "success" })))
 }
 
+#[cached(
+    ty = "TimedCache<(u64, bool, bool), bool>",
+    create = "{ TimedCache::with_lifespan(std::time::Duration::from_secs(60 * 60)) }",
+    result = true,
+    convert = "{ (match_id, metadata, replay) }",
+    sync_writes = "by_key",
+    key = "(u64, bool, bool)"
+)]
+pub(super) async fn has_salts_in_clickhouse(
+    ch_client: &clickhouse::Client,
+    match_id: u64,
+    metadata: bool,
+    replay: bool,
+) -> clickhouse::error::Result<bool> {
+    #[derive(Deserialize, Row)]
+    struct HasSalts {
+        has_metadata: bool,
+        has_replay: bool,
+    }
+    ch_client
+        .query(
+            "SELECT metadata_salt > 0 AS has_metadata, replay_salt > 0 AS has_replay FROM match_salts FINAL WHERE match_id = ?",
+        )
+        .bind(match_id)
+        .fetch_one::<HasSalts>()
+        .await
+        .map(|s| s.has_metadata && metadata || s.has_replay && replay)
+}
+
 async fn validate_salt(
     steam_client: &SteamClient,
+    ch_client: &clickhouse::Client,
     salt: &ClickhouseSalts,
 ) -> Option<ClickhouseSalts> {
     if salt.match_id > 100000000 {
         warn!("Match id too high, skipping");
         return None;
     }
+
+    let has_salts_already = has_salts_in_clickhouse(
+        ch_client,
+        salt.match_id,
+        salt.metadata_salt.is_some(),
+        salt.replay_salt.is_some(),
+    )
+    .await
+    .unwrap_or_default();
+    if has_salts_already {
+        return None;
+    }
+
     let mut salt = salt.clone();
     if salt.metadata_salt.is_some() {
         let is_valid = tryhard::retry_fn(|| steam_client.metadata_file_exists(&salt))
