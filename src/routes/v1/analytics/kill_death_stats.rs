fdf37832ee655160215bc7c12ece98b5a702288d
Manuel Raimann <raimannma@outlook.de>
2025-11-07 18:00:11 +0100

    feat: Add kill-death statistics endpoint with query parameters for filtering

---
+++ src/routes/v1/analytics/kill_death_stats.rs
@@ -0,0 +1,177 @@
+use axum::Json;
+use axum::extract::State;
+use axum::response::IntoResponse;
+use axum_extra::extract::Query;
+use clickhouse::Row;
+use serde::{Deserialize, Serialize};
+use tracing::debug;
+use utoipa::{IntoParams, ToSchema};
+
+use crate::context::AppState;
+use crate::error::APIResult;
+use crate::utils::parse::default_last_month_timestamp;
+
+fn default_min_kills_deaths() -> Option<u32> {
+    5.into()
+}
+
+#[derive(Copy, Debug, Clone, Deserialize, IntoParams, Eq, PartialEq, Hash)]
+pub(crate) struct KillDeathStatsQuery {
+    /// Filter matches based on their start time (Unix timestamp). **Default:** 30 days ago.
+    #[serde(default = "default_last_month_timestamp")]
+    #[param(default = default_last_month_timestamp)]
+    min_unix_timestamp: Option<i64>,
+    /// Filter matches based on their start time (Unix timestamp).
+    max_unix_timestamp: Option<i64>,
+    /// Filter matches based on their duration in seconds (up to 7000s).
+    #[param(maximum = 7000)]
+    min_duration_s: Option<u64>,
+    /// Filter matches based on their duration in seconds (up to 7000s).
+    #[param(maximum = 7000)]
+    max_duration_s: Option<u64>,
+    /// Filter matches based on whether they are in the high skill range.
+    is_high_skill_range_parties: Option<bool>,
+    /// Filter matches based on whether they are in the low priority pool.
+    is_low_pri_pool: Option<bool>,
+    /// Filter matches based on whether they are in the new player pool.
+    is_new_player_pool: Option<bool>,
+    /// Filter matches based on their ID.
+    min_match_id: Option<u64>,
+    /// Filter matches based on their ID.
+    max_match_id: Option<u64>,
+    /// Filter matches based on the average badge level (0-116) of *both* teams involved. See more: <https://assets.deadlock-api.com/v2/ranks>
+    #[param(minimum = 0, maximum = 116)]
+    min_average_badge: Option<u8>,
+    /// Filter matches based on the average badge level (0-116) of *both* teams involved. See more: <https://assets.deadlock-api.com/v2/ranks>
+    #[param(minimum = 0, maximum = 116)]
+    max_average_badge: Option<u8>,
+    /// Filter Raster cells based on minimum kills.
+    #[serde(default = "default_min_kills_deaths")]
+    #[param(default = default_min_kills_deaths, minimum = 1)]
+    min_kills_per_raster: Option<u32>,
+    /// Filter Raster cells based on minimum deaths.
+    #[serde(default = "default_min_kills_deaths")]
+    #[param(default = default_min_kills_deaths, minimum = 1)]
+    min_deaths_per_raster: Option<u32>,
+}
+
+#[derive(Debug, Clone, Row, Serialize, Deserialize, ToSchema)]
+pub(crate) struct KillDeathStats {
+    position_x: i32,
+    position_y: i32,
+    deaths: u64,
+    kills: u64,
+}
+
+fn build_query(query: &KillDeathStatsQuery) -> String {
+    let mut info_filters = vec![];
+    if let Some(min_unix_timestamp) = query.min_unix_timestamp {
+        info_filters.push(format!("start_time >= {min_unix_timestamp}"));
+    }
+    if let Some(max_unix_timestamp) = query.max_unix_timestamp {
+        info_filters.push(format!("start_time <= {max_unix_timestamp}"));
+    }
+    if let Some(min_match_id) = query.min_match_id {
+        info_filters.push(format!("match_id >= {min_match_id}"));
+    }
+    if let Some(max_match_id) = query.max_match_id {
+        info_filters.push(format!("match_id <= {max_match_id}"));
+    }
+    if let Some(min_badge_level) = query.min_average_badge
+        && min_badge_level > 11
+    {
+        info_filters.push(format!(
+            "average_badge_team0 >= {min_badge_level} AND average_badge_team1 >= {min_badge_level}"
+        ));
+    }
+    if let Some(max_badge_level) = query.max_average_badge
+        && max_badge_level < 116
+    {
+        info_filters.push(format!(
+            "average_badge_team0 <= {max_badge_level} AND average_badge_team1 <= {max_badge_level}"
+        ));
+    }
+    if let Some(max_duration_s) = query.max_duration_s {
+        info_filters.push(format!("duration_s <= {max_duration_s}"));
+    }
+    if let Some(is_high_skill_range_parties) = query.is_high_skill_range_parties {
+        info_filters.push(format!(
+            "is_high_skill_range_parties = {is_high_skill_range_parties}"
+        ));
+    }
+    if let Some(is_low_pri_pool) = query.is_low_pri_pool {
+        info_filters.push(format!("low_pri_pool = {is_low_pri_pool}"));
+    }
+    if let Some(is_new_player_pool) = query.is_new_player_pool {
+        info_filters.push(format!("new_player_pool = {is_new_player_pool}"));
+    }
+    let info_filters = if info_filters.is_empty() {
+        String::new()
+    } else {
+        format!(" AND {}", info_filters.join(" AND "))
+    };
+    format!(
+        "
+    WITH t_matches AS (SELECT match_id FROM match_info WHERE start_time > now() - interval 2 MONTH {info_filters}),
+         t_events AS (SELECT toInt32(round(tupleElement(dd.death_pos, 1), -2)) as position_x,
+                             toInt32(round(tupleElement(dd.death_pos, 2), -2)) as position_y,
+                             'death'                                  as type
+                      FROM match_player
+                               ARRAY JOIN death_details as dd
+                      WHERE match_id IN t_matches
+                      UNION ALL
+                      SELECT toInt32(round(tupleElement(dd.killer_pos, 1), -2)) as position_x,
+                             toInt32(round(tupleElement(dd.killer_pos, 2), -2)) as position_y,
+                             'kill'                                    as type
+                      FROM match_player
+                               ARRAY JOIN death_details as dd
+                      WHERE match_id IN t_matches)
+    SELECT position_x, position_y, count(type = 'death') as deaths, count(type = 'kill') as kills
+    FROM t_events
+    GROUP BY position_x, position_y
+    HAVING deaths > {} and kills > {}
+    ",
+        query.min_deaths_per_raster.unwrap_or(1),
+        query.min_kills_per_raster.unwrap_or(1)
+    )
+}
+
+async fn get_kill_death_stats(
+    ch_client: &clickhouse::Client,
+    query: KillDeathStatsQuery,
+) -> APIResult<Vec<KillDeathStats>> {
+    let query = build_query(&query);
+    debug!(?query);
+    Ok(ch_client.query(&query).fetch_all().await?)
+}
+
+#[utoipa::path(
+    get,
+    path = "/kill-death-stats",
+    params(KillDeathStatsQuery),
+    responses(
+        (status = OK, description = "Kill Death Stats", body = [KillDeathStats]),
+        (status = BAD_REQUEST, description = "Provided parameters are invalid."),
+        (status = INTERNAL_SERVER_ERROR, description = "Failed to fetch kill death stats")
+    ),
+    tags = ["Analytics"],
+    summary = "Kill Death Stats",
+    description = "
+This endpoint returns the kill-death statistics across a 100x100 pixel raster.
+
+### Rate Limits:
+| Type | Limit |
+| ---- | ----- |
+| IP | 100req/s |
+| Key | - |
+| Global | - |
+    "
+)]
+pub(crate) async fn kill_death_stats(
+    Query(query): Query<KillDeathStatsQuery>,
+    State(state): State<AppState>,
+) -> APIResult<impl IntoResponse> {
+    get_kill_death_stats(&state.ch_client_ro, query)
+        .await
+        .map(Json)
+}
