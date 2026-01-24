86b04c2482c4d64fcebefaaea839f3f41811e635
Manuel Raimann <raimannma@outlook.com>
2026-01-24 12:34:12 +0100

    feat: skip serializing username in match history struct

--- src/routes/v1/players/match_history.rs
+++ src/routes/v1/players/match_history.rs
@@ -54,7 +54,6 @@ pub(crate) struct PlayerMatchHistoryEntry {
     brawl_score_team0: Option<u32>,
     brawl_score_team1: Option<u32>,
     brawl_avg_round_time_s: Option<u32>,
-    #[serde(skip_serializing)]
     username: Option<String>,
 }
 
